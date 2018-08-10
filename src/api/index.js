8d2a90f5afc5db2dfdc71524eab12adb96f437f5
Khoa <lordknight@LKs-MacBook-Pro.local>
2018-08-10 11:10:02 +0900

    3

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import Api from './Api';
 
-// const api = new Api('https://emmapi.herokuapp.com');
-const api = new Api('http://127.0.0.1:4000');
+// const api = new Api('http://127.0.0.1:4000');
+const api = new Api('http://islab.snu.ac.kr:4000');
 export default api;
