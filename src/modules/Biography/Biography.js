a059fcab3e86e4d753dea9d1d3fc669ef2a7643e
Khoa Anh Ngo <lordknight1904@gmail.com>
2018-08-11 20:49:24 +0900

    11/8 4

--- src/modules/Biography/Biography.js
+++ src/modules/Biography/Biography.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
+import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchTitle, setHelmet } from '../../reducers/App/app';
-import { withStyles, Paper, Grid, Typography, Divider, Hidden } from '@material-ui/core';
+import {connect} from 'react-redux';
+import {fetchTitle, setHelmet} from '../../reducers/App/app';
+import {withStyles, Paper, Grid, Typography, Divider} from '@material-ui/core';
 import biographyStyle from "../../assets/jss/components/biographyStyle";
-import { withRouter } from "react-router-dom";
+import {withRouter} from "react-router-dom";
 import bioBshim from '../../../src/assets/images/bioBshim.png';
 import professor from '../../data/professor';
 
 class Biography extends Component {
   componentDidMount() {
-    const { app } = this.props;
+    const {app} = this.props;
     this.props.setHelmet(app.appName, app.appName);
   }
 
   render() {
-    const { classes } = this.props;
+    const {classes} = this.props;
     return (
       <div className={classes.pageContainer}>
         <Grid
@@ -28,67 +28,38 @@ class Biography extends Component {
           <Grid item md={8} xs={10}>
             <Paper className={classes.paperPadding} elevation={8}>
               <Grid container>
-                <Hidden mdDown>
-                  <Grid item md={8}>
-                    <Typography variant='headline' gutterBottom paragraph>
-                      {professor.name}
-                    </Typography>
-                    <Typography variant='title' gutterBottom>
-                      {professor.title}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom>
-                      {professor.university.department}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom paragraph>
-                      <a href={professor.university.URL} target="_blank">
-                        {professor.university.name}
-                      </a>
-                    </Typography>
-                    <Typography variant='title' gutterBottom>
-                      {professor.lab.title}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom paragraph>
-                      {professor.lab.name}
-                    </Typography>
-                  </Grid>
-                  <Grid item md={4} className={classes.imgWrap}>
-                    <img src={bioBshim} alt='loading' />
-                  </Grid>
-                </Hidden>
-                <Hidden mdUp>
-                  <Grid item md={4} className={classes.imgWrap}>
-                    <img src={bioBshim} alt='loading' />
-                  </Grid>
-                  <Grid item md={8}>
-                    <Typography variant='headline' gutterBottom paragraph>
-                      {professor.name}
-                    </Typography>
-                    <Typography variant='title' gutterBottom>
-                      {professor.title}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom>
-                      {professor.university.department}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom paragraph>
-                      <a href={professor.university.URL} target="_blank">
-                        {professor.university.name}
-                      </a>
-                    </Typography>
-                    <Typography variant='title' gutterBottom>
-                      {professor.lab.title}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom paragraph>
-                      {professor.lab.name}
-                    </Typography>
-                  </Grid>
-                </Hidden>
-                <Grid item md={12} style={{ marginTop: '20px' }}>
+                <Grid item md={4} className={classes.imgWrap}>
+                  <img src={bioBshim} alt='loading'/>
+                </Grid>
+                <Grid item md={8} style={{ paddingLeft: '20px' }}>
+                  <Typography variant='headline' gutterBottom paragraph>
+                    {professor.name}
+                  </Typography>
+                  <Typography variant='title' gutterBottom>
+                    {professor.title}
+                  </Typography>
+                  <Typography variant='subheading' gutterBottom>
+                    {professor.university.department}
+                  </Typography>
+                  <Typography variant='subheading' gutterBottom paragraph>
+                    <a href={professor.university.URL} target="_blank">
+                      {professor.university.name}
+                    </a>
+                  </Typography>
+                  <Typography variant='title' gutterBottom>
+                    {professor.lab.title}
+                  </Typography>
+                  <Typography variant='subheading' gutterBottom paragraph>
+                    {professor.lab.name}
+                  </Typography>
+                </Grid>
+                <Grid item md={12} style={{marginTop: '20px'}}>
                   <Typography variant='body1' align='justify'>
                     {professor.bio}
                   </Typography>
                 </Grid>
               </Grid>
-              <Divider style={{ marginTop: '20px', marginBottom: '20px' }} />
+              <Divider style={{marginTop: '20px', marginBottom: '20px'}}/>
               <Grid container>
                 <Grid item md={12}>
                   <a href={professor.curriculumVitae} target="_blank">
@@ -109,7 +80,7 @@ Biography.propTypes = {
   setHelmet: PropTypes.func.isRequired,
 };
 
-const mapStateToProps = ({ app }) => ({
+const mapStateToProps = ({app}) => ({
   app,
 });
 const mapDispatchToProps = dispatch =>
--- src/modules/Home/Home.js
+++ src/modules/Home/Home.js
@@ -3,7 +3,7 @@ import {bindActionCreators} from 'redux';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {fetchTitle, setHelmet} from '../../reducers/App/app';
-import {withStyles, Paper, Grid, Typography, Hidden} from '@material-ui/core';
+import {withStyles, Paper, Grid, Typography } from '@material-ui/core';
 import homeStyle from "../../assets/jss/components/homeStyle";
 import {withRouter} from "react-router-dom";
 import homeBshim from '../../../src/assets/images/homeBshim.jpg';
@@ -28,78 +28,40 @@ class Home extends Component {
           <Grid item md={8} xs={10}>
             <Paper className={classes.paperPadding}>
               <Grid container>
-                <Hidden only={['lg', 'xl', 'md']}>
-                  <Grid item md={4} className={classes.imgWrap}>
-                    <img src={homeBshim} alt='loading' width="300" height="400"/>
-                  </Grid>
-                  <Grid item md={8} style={{paddingLeft: '20px'}}>
-                    <Typography variant='headline' gutterBottom paragraph>
-                      {professor.name}
-                    </Typography>
-                    <Typography variant='title' gutterBottom>
-                      {professor.title}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom>
-                      {professor.university.department}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom paragraph>
-                      <a href={professor.university.URL} target="_blank">
-                        {professor.university.name}
-                      </a>
-                    </Typography>
-                    <Typography variant='title' gutterBottom>
-                      {professor.lab.title}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom paragraph>
-                      {professor.lab.name}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom paragraph>
-                      {professor.workAddress}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom paragraph>
-                      Tel: {professor.phone}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom paragraph>
-                      Email: {professor.email}
-                    </Typography>
-                  </Grid>
-                </Hidden>
-                <Hidden only={['xs', 'sm']}>
-                  <Grid item md={8}>
-                    <Typography variant='headline' gutterBottom paragraph>
-                      {professor.name}
-                    </Typography>
-                    <Typography variant='title' gutterBottom>
-                      {professor.title}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom>
-                      {professor.university.department}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom paragraph>
-                      <a href={professor.university.URL} target="_blank">
-                        {professor.university.name}
-                      </a>
-                    </Typography>
-                    <Typography variant='title' gutterBottom>
-                      {professor.lab.title}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom paragraph>
-                      {professor.lab.name}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom paragraph>
-                      {professor.workAddress}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom paragraph>
-                      Tel: {professor.phone}
-                    </Typography>
-                    <Typography variant='subheading' gutterBottom paragraph>
-                      Email: {professor.email}
-                    </Typography>
-                  </Grid>
-                  <Grid item md={4} className={classes.imgWrap}>
-                    <img src={homeBshim} alt='loading' width="300" height="400"/>
-                  </Grid>
-                </Hidden>
+                <Grid item md={4} className={classes.imgWrap}>
+                  <img src={homeBshim} alt='loading' width="300" height="400"/>
+                </Grid>
+                <Grid item md={8} style={{ paddingLeft: '20px' }}>
+                  <Typography variant='headline' gutterBottom paragraph>
+                    {professor.name}
+                  </Typography>
+                  <Typography variant='title' gutterBottom>
+                    {professor.title}
+                  </Typography>
+                  <Typography variant='subheading' gutterBottom>
+                    {professor.university.department}
+                  </Typography>
+                  <Typography variant='subheading' gutterBottom paragraph>
+                    <a href={professor.university.URL} target="_blank">
+                      {professor.university.name}
+                    </a>
+                  </Typography>
+                  <Typography variant='title' gutterBottom>
+                    {professor.lab.title}
+                  </Typography>
+                  <Typography variant='subheading' gutterBottom paragraph>
+                    {professor.lab.name}
+                  </Typography>
+                  <Typography variant='subheading' gutterBottom paragraph>
+                    {professor.workAddress}
+                  </Typography>
+                  <Typography variant='subheading' gutterBottom paragraph>
+                    Tel: {professor.phone}
+                  </Typography>
+                  <Typography variant='subheading' gutterBottom paragraph>
+                    Email: {professor.email}
+                  </Typography>
+                </Grid>
                 <Grid item md={12} style={{marginTop: '20px'}}>
                   <Typography variant='subheading' gutterBottom paragraph align='justify'>
                     {professor.description}
